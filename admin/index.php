f05d1fbae51d201585190108832b389634df5726
adzkarfauzie <adzkarfauzie02@gmail.com>
2017-10-30 08:00:06 +0700

    Halaman Admin

--- admin/index.php
+++ admin/index.php
@@ -2,6 +2,7 @@
 	session_start();
 	include_once '../config/web.php';
 	//cek session apakah sudah login atau blum
+	//jika sudah akan langsung terkirim ke halaman dashboard
 	if (@$_SESSION["status_login"] == "sudah") {
 		header("location: {$url}admin/dashboard");
 	} else {
